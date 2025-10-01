11aa4fcd4a74ddac368ed1ca1719aaada60ba555
Felix Patzschke <felix.patzschke@gmail.com>
2025-10-01 11:01:18 +0200

    add project config files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ptirtools",
+    version="0.1.0",
+    description="PTIR Tools",
+    author="Felix H. Patzschke",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    install_requires=[
+        "h5py>=3.14",
+        "numpy>=2.2",
+        "scipy>=1.15",
+        "matplotlib>=3.10",
+        "re>=2.2"
+    ]
+)
