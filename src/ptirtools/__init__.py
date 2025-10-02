361a77aa339c4dba6ad98fcd9e2939021dcee19b
Felix Patzschke <felix.patzschke@gmail.com>
2025-10-02 09:55:11 +0200

    restructure __init__.py

--- src/ptirtools/__init__.py
+++ src/ptirtools/__init__.py
@@ -2,12 +2,13 @@
 
 __version__ = "0.1.0"
 
-# Add core functionality here
-
+# expose sub-modules here
 import ptirtools.datasets
-from ptirtools.datasets import Dataset, Image, Heightmap, Measurement
-
 import ptirtools.plotting
 import ptirtools.dicttools
 import ptirtools.peaks
+import ptirtools.debugging
+
+# expose core functionality here
+from ptirtools.datasets import Dataset, Image, Heightmap, Measurement
 
