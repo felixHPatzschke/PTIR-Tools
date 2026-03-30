214b4ed1ef55d24975705882e0c6bbc3184986eb
Felix Patzschke <felix.patzschke@gmail.com>
2026-03-30 16:58:49 +0200

    define some handy string constants

---
+++ src/ptirtools/misc/unicode.py
@@ -0,0 +1,16 @@
+
+"""
+Here, we define some string constants that come in handy in the evaluation of IR data and are cumbersome to type.
+"""
+
+WAVENUMBERS = "cm⁻¹"
+CM_1 = "cm⁻¹"
+
+STRETCH = "ν"
+BEND = "δ"
+TWIST = "τ"
+ROCK = "ρ"
+WAG = "γ"
+
+SYM = "ₛ"
+ASYM = "ₐₛ"
